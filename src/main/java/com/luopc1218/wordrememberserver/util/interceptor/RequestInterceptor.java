3036a388a45b71c5c38fa02866b249419773591e
mawendaxiong <455766076@qq.com>
2022-04-26 15:24:14 +0800

    fix: fix response

--- src/main/java/com/luopc1218/wordrememberserver/util/interceptor/RequestInterceptor.java
+++ src/main/java/com/luopc1218/wordrememberserver/util/interceptor/RequestInterceptor.java
@@ -1,10 +1,15 @@
 package com.luopc1218.wordrememberserver.util.interceptor;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.luopc1218.wordrememberserver.entity.ApiResponse;
 import com.luopc1218.wordrememberserver.util.annotation.JsonWebTokenRequire;
+import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.io.IOException;
 
 public class RequestInterceptor implements HandlerInterceptor {
     @Override
@@ -16,6 +21,18 @@ public class RequestInterceptor implements HandlerInterceptor {
             return true;
         }
         System.out.println("need sign in!");
+        ApiResponse fail = ApiResponse.fail("need sign in!");
+        fail.setCode(401);
+        try {
+            ServletOutputStream outputStream = response.getOutputStream();
+            ObjectMapper objectMapper = new ObjectMapper();
+            byte[] bytes = objectMapper.writeValueAsBytes(fail);
+            outputStream.write(bytes);
+            response.flushBuffer();
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 }
